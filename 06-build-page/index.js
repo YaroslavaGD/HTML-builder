49d616b79540915bd29cbe45f5da147083655f70
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-05-09 13:50:26 +0200

    feat: implement dist-creation in 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -7,7 +7,7 @@ const assetsPath = path.join(__dirname, 'assets');
 const resultAssetsPath = path.join(resultFolderPath, 'assets');
 
 const componentsPath = path.join(__dirname, 'components');
-const resultComponentsPath = path.join(resultFolderPath, 'components');
+// const resultComponentsPath = path.join(resultFolderPath, 'components');
 
 const stylesPath = path.join(__dirname, 'styles');
 const resultStylesPath = path.join(resultFolderPath, 'style.css');
@@ -15,3 +15,35 @@ const resultStylesPath = path.join(resultFolderPath, 'style.css');
 
 const templateHtmlPath = path.join(__dirname, 'template.html');
 
+async function deleteFolder() {
+  try {
+    await fs.promises.access(resultFolderPath);
+    await fs.promises.rm(resultFolderPath, {recursive: true});
+  } catch (err) {
+    return false;
+  }
+}
+
+async function createFolder() {
+  try {
+    await fs.promises.mkdir(resultFolderPath, {recursive: true});
+    await fs.promises.mkdir(resultAssetsPath, {recursive: true});
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function buildPage() {
+  try {
+    await deleteFolder();
+    await createFolder();
+  } catch (err) {
+    console.log("main err : " + err);
+  }
+}
+
+async function copyAssetsFiles() {
+  
+}
+
+buildPage();
