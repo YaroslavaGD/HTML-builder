28ebbb19063c02d463e6db8d061c049a7eef8065
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-05-09 13:08:42 +0200

    refactor: change then() to await in 05-merge-styles

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,44 +2,6 @@ const { stdin, stdout} = require("process");
 const fs = require('fs');
 const path = require('path');
 
-// // создаем папку, если нужно
-// fs.mkdir(
-//   path.join(__dirname, 'files-copy'),
-//   {recursive: true},
-//   err => {
-//     if (err) throw new Error("Папка files-copy уже существует или другая ошибка");
-  
-//     console.log('Папка files-copy создана');
-//   }
-// );
-
-// // удаляем то что раньше там лежало, если там что-то было
-// fs.promises.readdir(
-//   path.join(__dirname, 'files-copy')
-// ).then( files => {
-//   files.forEach(file => {
-//     fs.unlink(
-//       path.join(__dirname, 'files-copy', file),
-//       err => {
-//         if (err) throw err;
-//       }
-//     );
-//   })
-// });
-
-// // копируем все файлы из исходной папки
-// fs.promises.readdir(
-//   path.join(__dirname, 'files')
-// ).then( files => {
-//   files.forEach(file => {
-//     fs.promises.copyFile(
-//       path.join(__dirname,'files', file),
-//       path.join(__dirname, 'files-copy', file)
-//     );
-//   });
-
-// });
-
 copyFolder();
 
 async function copyFolder() {
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,29 +2,38 @@ const { stdin, stdout} = require("process");
 const fs = require('fs');
 const path = require('path');
 
-const resultFile = fs.createWriteStream(
-  path.join(__dirname, 'project-dist', 'bundle.css')
-);
+mergeStyles();
 
-fs.promises.readdir(
-  path.join(__dirname, 'styles'),
-  {withFileTypes: true}
-).then( files => {
-  files.forEach( file => {
-    if (!file.isDirectory()) {
-      let filePath = path.join(__dirname, 'styles', file.name);
-      let fileName = path.basename(filePath);
-      let fileExtension = path.extname(filePath);
+async function mergeStyles() {
+  try {
+    const resultFile = fs.createWriteStream(
+      path.join(__dirname, 'project-dist', 'bundle.css')
+    );
 
-      if (fileExtension === '.css') {
-        const sourceFile = fs.createReadStream(
-          path.join(__dirname, 'styles', fileName)
-        );
+    const files = await fs.promises.readdir(
+      path.join(__dirname, 'styles'), 
+      {withFileTypes: true}
+    );
 
-        sourceFile.on('data', data => {
-          resultFile.write(data.toString() + '\n');
-        });
+    files.forEach(file => {
+      if (!file.isDirectory()) {
+        let filePath = path.join(__dirname, 'styles', file.name);
+        let fileName = path.basename(filePath);
+        let fileExtension = path.extname(filePath);
+  
+        if (fileExtension === '.css') {
+          const sourceFile = fs.createReadStream(
+            path.join(__dirname, 'styles', fileName)
+          );
+  
+          sourceFile.on('data', data => {
+            resultFile.write(data.toString() + '\n');
+          });
+        }
       }
-    }
-  })
-});
+    });
+
+  } catch (err) {
+    console.log("main error: " + err);
+  }
+}
